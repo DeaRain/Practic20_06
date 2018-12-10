fc9919d54990d7ea6e20cc3f36dc0240afe2643e
DeaRain <steallmaster@gmail.com>
2018-12-11 01:37:31 +0200

    Update Profile.php

--- modules/user/models/Profile.php
+++ modules/user/models/Profile.php
@@ -9,9 +9,15 @@ use Yii;
  *
  * @property int $id
  * @property string $username
- * @property string $password
- * @property int $banned
  * @property string $auth_key
+ * @property string $password_hash
+ * @property string $password_reset_token
+ * @property string $email
+ * @property int $status
+ * @property int $created_at
+ * @property int $updated_at
+ *
+ * @property Article[] $articles
  */
 class Profile extends \yii\db\ActiveRecord
 {
@@ -23,4 +29,45 @@ class Profile extends \yii\db\ActiveRecord
         return 'user';
     }
 
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'auth_key', 'password_hash', 'email', 'created_at', 'updated_at'], 'required'],
+            [['status', 'created_at', 'updated_at'], 'integer'],
+            [['username', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['username'], 'unique'],
+            [['email'], 'unique'],
+            [['password_reset_token'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'auth_key' => 'Auth Key',
+            'password_hash' => 'Password Hash',
+            'password_reset_token' => 'Password Reset Token',
+            'email' => 'Email',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticles()
+    {
+        return $this->hasMany(Article::className(), ['author' => 'id']);
+    }
 }
